81086362e7e907cfeeea9f85073927680b447ab7
Daniel Audino <daniel@skrumble.com>
2017-02-08 17:23:41 -0500

    Collapsed if statements

--- index.js
+++ index.js
@@ -36,45 +36,19 @@ Voxbone.prototype = {
 					url += "&didgroupIds="+didgroupId;
 				});
 			}
-			if (opts.e164Pattern) {
-				url += "&e164Pattern="+opts.e164Pattern;
-			}
-			if (opts.regulationAddressId) {
-				url += "&regulationAddressId="+opts.regulationAddressId;
-			}
-			if (opts.voiceUriId) {
-				url += "&voiceUriId="+opts.voiceUriId;
-			}
-			if (opts.faxUriId) {
-				url += "&faxUriId="+opts.faxUriId;
-			}
-			if (opts.smsLinkGroupId) {
-				url += "&smsLinkGroupId="+opts.smsLinkGroupId;
-			}
-			if (opts.needAddressLink) {
-				url += "&needAddressLink="+opts.needAddressLink;
-			}
-			if (opts.serviceType) {
-				url += "&serviceType="+opts.serviceType;
-			}
-			if (opts.countryCodeA3) {
-				url += "&countryCodeA3="+opts.countryCodeA3;
-			}
-			if (opts.orderReference) {
-				url += "&orderReference="+opts.orderReference;
-			}
-			if (opts.portingReference) {
-				url += "&portingReference="+opts.portingReference;
-			}
-			if (opts.deliveryId) {
-				url += "&deliveryId="+opts.deliveryId;
-			}
-			if (opts.smsOutbound) {
-				url += "&smsOutbound="+opts.smsOutbound;
-			}
-			if (opts.webRtcEnabled) {
-				url += "&webRtcEnabled="+opts.webRtcEnabled;
-			}
+			if (opts.e164Pattern) url += "&e164Pattern="+opts.e164Pattern;
+			if (opts.regulationAddressId) url += "&regulationAddressId="+opts.regulationAddressId;
+			if (opts.voiceUriId) url += "&voiceUriId="+opts.voiceUriId;
+			if (opts.faxUriId) url += "&faxUriId="+opts.faxUriId;
+			if (opts.smsLinkGroupId) url += "&smsLinkGroupId="+opts.smsLinkGroupId;
+			if (opts.needAddressLink) url += "&needAddressLink="+opts.needAddressLink;
+			if (opts.serviceType) url += "&serviceType="+opts.serviceType;
+			if (opts.countryCodeA3) url += "&countryCodeA3="+opts.countryCodeA3;
+			if (opts.orderReference) url += "&orderReference="+opts.orderReference;
+			if (opts.portingReference) url += "&portingReference="+opts.portingReference;
+			if (opts.deliveryId) url += "&deliveryId="+opts.deliveryId;
+			if (opts.smsOutbound) url += "&smsOutbound="+opts.smsOutbound;
+			if (opts.webRtcEnabled) url += "&webRtcEnabled="+opts.webRtcEnabled;
 
 			resolve(sendRequest("GET", url));
 		});
@@ -86,12 +60,8 @@ Voxbone.prototype = {
 			}
 			var url = _api.url+"inventory/country?pageNumber="+opts.pageNumber+"&pageSize="+opts.pageSize;
 
-			if (opts.countryCodeA3) {
-				url += "&countryCodeA3="+opts.countryCodeA3;
-			}
-			if (opts.didType) {
-				url += "&didType="+opts.didType;
-			}
+			if (opts.countryCodeA3) url += "&countryCodeA3="+opts.countryCodeA3;
+			if (opts.didType) url += "&didType="+opts.didType;
 
 			resolve(sendRequest("GET", url));
 		});
@@ -103,21 +73,11 @@ Voxbone.prototype = {
 			}
 			var url = _api.url+"configuration/voiceuri?pageNumber="+opts.pageNumber+"&pageSize="+opts.pageSize;
 
-			if (opts.voiceUriId) {
-				url += "&voiceUriId="+opts.voiceUriId;
-			}
-			if (opts.backupUriId) {
-				url += "&backupUriId="+opts.backupUriId;
-			}
-			if (opts.voiceUriProtocol) {
-				url += "&voiceUriProtocol="+opts.voiceUriProtocol;
-			}
-			if (opts.uri) {
-				url += "&uri="+opts.uri;
-			}
-			if (opts.description) {
-				url += "&description="+opts.description;
-			}
+			if (opts.voiceUriId) url += "&voiceUriId="+opts.voiceUriId;
+			if (opts.backupUriId) url += "&backupUriId="+opts.backupUriId;
+			if (opts.voiceUriProtocol) url += "&voiceUriProtocol="+opts.voiceUriProtocol;
+			if (opts.uri) url += "&uri="+opts.uri;
+			if (opts.description) url += "&description="+opts.description;
 
 			resolve(sendRequest("GET", url));
 		});
@@ -134,15 +94,9 @@ Voxbone.prototype = {
 				uri: uri.uri
 			};
 
-			if (uri.voiceUriId) {
-				body.voiceUriId = uri.voiceUriId;
-			}
-			if (uri.backupUriId) {
-				body.backupUriId = uri.backupUriId;
-			}
-			if (uri.description) {
-				body.description = uri.description;
-			}
+			if (uri.voiceUriId) body.voiceUriId = uri.voiceUriId;
+			if (uri.backupUriId) body.backupUriId = uri.backupUriId;
+			if (uri.description) body.description = uri.description;
 
 			resolve(sendRequest("PUT", url, body));
 		});
@@ -158,63 +112,26 @@ Voxbone.prototype = {
 				didIds: config.didIds
 			};
 
-			if (config.voiceUriId) {
-				body.voiceUriId = config.voiceUriId;
-			}
-			if (config.smsLinkGroupId) {
-				body.smsLinkGroupId = config.smsLinkGroupId;
-			}
-			if (config.faxUriId) {
-				body.faxUriId = config.faxUriId;
-			}
-			if (config.capacityGroupId) {
-				body.capacityGroupId = config.capacityGroupId;
-			}
-			if (config.trunkId) {
-				body.trunkId = config.trunkId;
-			}
-			if (config.deliveryId) {
-				body.deliveryId = config.deliveryId;
-			}
-			if (config.srvLookup) {
-				body.srvLookup = config.srvLookup;
-			}
-			if (config.cliPrivacy) {
-				body.cliPrivacy = config.cliPrivacy;
-			}
-			if (config.ringback) {
-				body.ringback = config.ringback;
-			}
-			if (config.dnisEnabled) {
-				body.dnisEnabled = config.dnisEnabled;
-			}
-			if (config.blockOrdinary) {
-				body.blockOrdinary = config.blockOrdinary;
-			}
-			if (config.blockCellular) {
-				body.blockCellular = config.blockCellular;
-			}
-			if (config.blockPayphone) {
-				body.blockPayphone = config.blockPayphone;
-			}
-			if (config.smsOutbound) {
-				body.smsOutbound = config.smsOutbound;
-			}
-			if (config.webRtcEnabled) {
-				body.webRtcEnabled = config.webRtcEnabled;
-			}
-			if (config.voxFaxEnabled) {
-				body.voxFaxEnabled = config.voxFaxEnabled;
-			}
-			if (config.limitChannels) {
-				body.limitChannels = config.limitChannels;
-			}
-			if (config.peer) {
-				body.peer = config.peer;
-			}
-			if (config.callerId) {
-				body.callerId = config.callerId;
-			}
+			if (config.voiceUriId) body.voiceUriId = config.voiceUriId;
+			if (config.smsLinkGroupId) body.smsLinkGroupId = config.smsLinkGroupId;
+			if (config.faxUriId) body.faxUriId = config.faxUriId;
+			if (config.capacityGroupId) body.capacityGroupId = config.capacityGroupId;
+			if (config.trunkId) body.trunkId = config.trunkId;
+			if (config.deliveryId) body.deliveryId = config.deliveryId;
+			if (config.srvLookup) body.srvLookup = config.srvLookup;
+			if (config.cliPrivacy) body.cliPrivacy = config.cliPrivacy;
+			if (config.ringback) body.ringback = config.ringback;
+			if (config.dnisEnabled) body.dnisEnabled = config.dnisEnabled;
+			if (config.blockOrdinary) body.blockOrdinary = config.blockOrdinary;
+			if (config.blockCellular) body.blockCellular = config.blockCellular;
+			if (config.blockPayphone) body.blockPayphone = config.blockPayphone;
+			if (config.smsOutbound) body.smsOutbound = config.smsOutbound;
+			if (config.webRtcEnabled) body.webRtcEnabled = config.webRtcEnabled;
+			if (config.voxFaxEnabled) body.voxFaxEnabled = config.voxFaxEnabled;
+			if (config.limitChannels) body.limitChannels = config.limitChannels;
+			if (config.peer) body.peer = config.peer;
+			if (config.callerId) body.callerId = config.callerId;
+
 			resolve(sendRequest("POST", url, body));
 		});
 	},
